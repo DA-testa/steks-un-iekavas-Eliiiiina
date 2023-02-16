a445a57edece8331ed9e4f1480be3ad61bc97908
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-02-16 14:57:10 +0000

    testins

--- main.py
+++ main.py
@@ -12,36 +12,32 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next_char in enumerate(text):
-        if next_char in "([{":
+    for i, next in enumerate(text):
+        if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(Bracket(next_char,i+1))
+            opening_brackets_stack.append(Bracket(next,i+1))
             #pass
 
-        if next_char in ")]}":
+        if next in ")]}":
             # Process closing bracket, write your code here
-           if(not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next_char)):
+           if(not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next)):
                 return i+1
             opening_brackets_stack.pop()
            
             if opening_brackets_stack:
                 return opening_brackets_stack[-1].position
-                return "Success" 
+        return "Success" 
      #pass
 
  # Printing answer, write your code here
 def main():
     choice= input()
-    if choice=choise.upper()=="F":
-         filename=input()
-           with open(filename,"r") as f:
-             text=f.read().strip()
-       elif choice.upper()=="I":
-        text=input()
-       else: print("Invalid choice")
-    return
-mismatch=find_mismatch(text)
-print(mismatch)
+    text=input()
+    mismatch= find_mismatch(text)
+    if choice=choise.upper()=="I":
+      print (mismatch)
+    else:
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
