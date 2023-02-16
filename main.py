59ecacf6fe48c11abf4497bd0652e0a7112976f5
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-02-16 09:49:59 +0000

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # python3
+#Elīna Miltiņa 221RDC017
 
 from collections import namedtuple
 
@@ -14,18 +15,34 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(next)
+            #pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack:
+                return i+1
+            if (next==')' and opening_brackets_stack[-1]!='(') or \
+               (next=='}' and opening_brackets_stack[-1]!='{') or \
+               (next==']' and opening_brackets_stack[-1]!='['):
+                 return i+1
+        opening_brackets_stack.pop()
+        if not opening_brackets_stack:
+            return "Success"
+        else:
+            return opening_brackets_stack[-1][1]
+
+            #pass
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    if isinstance(mismatch,int):
+        print(mismatch)
+    else:
+        print("Success")
 
 if __name__ == "__main__":
     main()
