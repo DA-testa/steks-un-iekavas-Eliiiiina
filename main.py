cbc1b542aeca9136531f1f08ffd620936f0885e9
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-02-19 18:30:13 +0000

    testiii

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def find_mismatch(text):
         if next in "([{":
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next,i+1))
-            pass
+            #pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
@@ -25,20 +25,31 @@ def find_mismatch(text):
         opening_brackets_stack.pop()
            
         if opening_brackets_stack:
-            return opening_brackets_stack[-1].position
+            return opening_brackets_stack[0].position
         return "Success" 
-     pass
+     #pass
 
  # Printing answer, write your code here
+def get_input():
+    while True:
+        user_input = input("Choose input type - F for file input or I for user input: ").upper()
+        if user_input == "F":
+            file_path = input("Enter file path: ")
+            if os.path.isfile(file_path):
+                with open(file_path, "r") as file:
+                    return file.read()
+            else:
+                print("File does not exist. Try again.")
+        elif user_input == "I":
+            user_brackets = input("Enter brackets: ")
+            return user_brackets.replace("\n", "")
+        else:
+            print("Invalid input. Try again.")
+
 def main():
-    choice= input()
-    text=input()
-    mismatch= find_mismatch(text)
-    choice.upper()
-    if choice == "I":
-      print (mismatch)
-    else:
-        print(mismatch)
+    text = get_input()
+    mismatch = find_mismatch(text)
+    print(mismatch)
 
 if __name__ == "__main__":
     main()
