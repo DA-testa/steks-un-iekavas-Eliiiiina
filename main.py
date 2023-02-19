c8cd594d95653c569f2c7d839d66e65e010b908b
Elīna Miltiņa <123946938+Eliiiiina@users.noreply.github.com>
2023-02-19 17:25:31 +0000

    testins

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def find_mismatch(text):
         if next in "([{":
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next,i+1))
-            #pass
+            pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
@@ -27,7 +27,7 @@ def find_mismatch(text):
         if opening_brackets_stack:
             return opening_brackets_stack[-1].position
         return "Success" 
-     #pass
+     pass
 
  # Printing answer, write your code here
 def main():
